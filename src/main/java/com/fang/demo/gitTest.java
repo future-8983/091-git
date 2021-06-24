549112b068b2f744b91839568912d7e375b1e9bb
fang <fang@fang.com>
2021-06-24 16:51:29 +0800

    master 1

--- src/main/java/com/fang/demo/gitTest.java
+++ src/main/java/com/fang/demo/gitTest.java
@@ -6,6 +6,7 @@ public class gitTest {
         System.out.println("这是一个新项目！");
         System.out.println("这是第二次的提交！");
         System.out.println("这是hot-fix提交！");
+        System.out.println("这是master 1");
     }
 
 }
