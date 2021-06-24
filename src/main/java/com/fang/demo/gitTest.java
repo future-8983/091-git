93462242b9c53ac9a6fd6da83419c102b10e06ec
fang <fang@fang.com>
2021-06-24 16:44:35 +0800

    third commit

--- src/main/java/com/fang/demo/gitTest.java
+++ src/main/java/com/fang/demo/gitTest.java
@@ -5,6 +5,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("这是一个新项目！");
         System.out.println("这是第二次的提交！");
+        System.out.println("这是hot-fix提交！");
     }
 
 }
